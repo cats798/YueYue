4b616f461c4ec6612a8a76d858469fff5ed7b560
卡茨特 <liyuanlin959@gmail.com>
2026-03-23 04:48:38 +0800

    删除 Source.swift

--- YueYue/Models/Source.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-import CoreData
-
-@objc(Source)
-public class Source: NSManagedObject {
-    @NSManaged public var name: String?
-    @NSManaged public var type: String?
-    @NSManaged public var ruleData: Data?
-}
