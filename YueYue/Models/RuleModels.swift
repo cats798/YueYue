9499b65a12dce4c4eeba18f558f4eb12e86a9838
卡茨特 <liyuanlin959@gmail.com>
2026-03-24 00:00:32 +0800

    更新 RuleModels.swift

--- YueYue/Models/RuleModels.swift
+++ YueYue/Models/RuleModels.swift
@@ -8,10 +8,13 @@ struct Rule: Codable {
     let searchRule: SearchRule
     let chapterRule: ChapterRule
     let contentRule: ContentRule
+    let discover: DiscoverRule?  // 可选，用于分类发现
 }
 
 struct SearchRule: Codable {
     let url: String
+    let method: String?          // GET 或 POST，默认 GET
+    let body: String?            // POST 时的 body 模板，使用 %@ 占位关键词
     let list: String
     let title: String
     let urlAttr: String
@@ -26,4 +29,11 @@ struct ChapterRule: Codable {
 struct ContentRule: Codable {
     let selector: String
     let text: String          // "text" 或 "html"
+}
+
+struct DiscoverRule: Codable {
+    let categories: [String: String]
+    let list: String
+    let title: String
+    let urlAttr: String
 }
